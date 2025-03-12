24761dd775be31cde5ce424e11deb01e002c19af
ackermanrafandra <ackermanrafandra@gmail.com>
2025-03-12 22:17:06 +0000

    Submit tugas Week2/part1/soal1.js

--- quiz/part1/soal1.js
+++ quiz/part1/soal1.js
@@ -9,7 +9,7 @@ let input = [
     ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
 ]
 function dataHandling(input) {
-    for (i = 0; i < input.length; i++) {
+    for (let i = 0; i < input.length; i++) {
         console.log(`Nomor ID: ${input[i][0]}`);
         console.log(`Nama Lengkap: ${input[i][1]}`);
         console.log(`TTL: ${input[i][2]} ${input[i][3]}`);
